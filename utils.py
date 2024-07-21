f5e74c30378d97f2d78c5c9166c6778eb6af23ef
Sebastian <sebastian_fis@yahoo.de>
2024-07-21 17:20:25 +0200

    re-implemented decode_bbox function

--- utils.py
+++ utils.py
@@ -101,9 +101,9 @@ def decode_bbox(preds, img_shape):
     strides = [
         int(np.sqrt(img_shape[-2] * img_shape[-1] / preds[p].shape[1])) for p in pos if preds[p].shape[2] != 64]
     for i, s in enumerate(strides):
-        print("s: " + str(s))
         if s == 0:
             strides[i] = 1
+        print("s: " + str(s))
 
     dims = [(img_h // s, img_w // s) for s in strides]
     fake_feats = [np.zeros((1, 1, h, w)) for h, w in dims]

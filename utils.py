9b41c2de6ef27c9c527dd88b92f1588906fc6ffd
Sebastian <sebastian_fis@yahoo.de>
2024-07-31 23:02:29 +0200

    Implemented Seperate output Decoder class instead of separate functions -> many calculations must be done only once!

--- utils.py
+++ utils.py
@@ -152,49 +152,6 @@ class Seperate_Output_Decoder:
                          dim=1) * self.strides  # Placeholder for dist2bbox function
 
         return np.concatenate((dbox, 1 / (1 + np.exp(-x[:, -self.num_classes:, :]))), axis=1)
-    
-
-def decode_bbox(preds, img_shape):
-    num_classes = next((o.shape[2] for o in preds if o.shape[2] != 64), -1)
-    assert num_classes != -1, 'cannot infer postprocessor inputs via output shape if there are 64 classes'
-    pos = [
-        i for i, _ in sorted(enumerate(preds),
-                             key=lambda x: (x[1].shape[2] if num_classes > 64 else -x[1].shape[2], -x[1].shape[1]))]
-    x = np.transpose(
-        np.concatenate([
-            np.concatenate([preds[i] for i in pos[:len(pos) // 2]], axis=1),
-            np.concatenate([preds[i] for i in pos[len(pos) // 2:]], axis=1)], axis=2), axes=(0, 2, 1))
-    reg_max = (x.shape[1] - num_classes) // 4
-    img_h, img_w = img_shape[-3], img_shape[-2]
-    # for i, i_shape in enumerate(img_shape):
-    #     print("img (" + str(i) + "): " + str(i_shape))
-    #
-    # for p in pos:
-    #     print("p: " + str(p))
-    #     print("preds[p].shape[1]: " + str(preds[p].shape[1]))
-    #     print("preds[p].shape[2]: " + str(preds[p].shape[2]))
-
-    strides = []
-    for p in pos:
-        if preds[p].shape[2] != 64:
-            strides.append(int(np.sqrt(img_h * img_w / preds[p].shape[1])))
-
-    for i, s in enumerate(strides):
-        print("s: " + str(strides[i]))
-
-    dims = [(img_h // s, img_w // s) for s in strides]
-    print(dims)
-    fake_feats = [np.zeros((1, 1, h, w)) for h, w in dims]
-    anchors, strides = (np.transpose(x, (1, 0))
-                        for x in make_anchors(fake_feats, strides, 0.5))  # Placeholder for make_anchors function
-
-    dbox = dist2bbox(dfl(x[:, :-num_classes, :], reg_max), anchors, xywh=True,
-                     dim=1) * strides  # Placeholder for dist2bbox function
-
-    return np.concatenate((dbox, 1 / (1 + np.exp(-x[:, -num_classes:, :]))), axis=1)
-
-
-
 
 def softmax(x, axis):
     """

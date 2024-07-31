corrected stride estimation vector calculation

edgetpumodel.py
@@ -129,7 +129,7 @@ class EdgeTPUModel:
         full_image, net_image, pad = get_image_tensor(image_path, self.input_size[0])
         pred = self.forward(net_image)
         if self.sep_output:
-            pred = decode_bbox(pred, net_image.shape[1:])
+            pred = decode_bbox(pred, net_image.shape)
         
         base, ext = os.path.splitext(image_path)
         
@@ -177,7 +177,7 @@ class EdgeTPUModel:
         if self.v8:
             result = np.transpose(result, [0, 2, 1])  # transpose for yolov8 models
         if self.sep_output:
-            result = decode_bbox(result, x.shape[1:])
+            result = decode_bbox(result, x.shape)
 
         self.inference_time = time.time() - tstart
         

utils.py
@@ -92,7 +92,7 @@ def decode_bbox(preds, img_shape):
             np.concatenate([preds[i] for i in pos[:len(pos) // 2]], axis=1),
             np.concatenate([preds[i] for i in pos[len(pos) // 2:]], axis=1)], axis=2), axes=(0, 2, 1))
     reg_max = (x.shape[1] - num_classes) // 4
-    img_h, img_w = img_shape[-2], img_shape[-1]
+    img_h, img_w = img_shape[-3], img_shape[-2]
     for i, i_shape in enumerate(img_shape):
         print("img (" + str(i) + "): " + str(i_shape))
 
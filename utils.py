corrected stride estimation vector calculation

utils.py
@@ -93,6 +93,8 @@ def decode_bbox(preds, img_shape):
             np.concatenate([preds[i] for i in pos[len(pos) // 2:]], axis=1)], axis=2), axes=(0, 2, 1))
     reg_max = (x.shape[1] - num_classes) // 4
     img_h, img_w = img_shape[-2], img_shape[-1]
+    print("img_h: " + str(img_h))
+    print("img_w: " + str(img_w))
     for p in pos:
         print("p: " + str(p))
         print("preds[p].shape[1]: " + str(preds[p].shape[1]))
@@ -101,7 +103,7 @@ def decode_bbox(preds, img_shape):
     strides = []
     for p in pos:
         if preds[p].shape[2] != 64:
-            strides.append(int(np.sqrt(img_shape[-2] * img_shape[-1] / preds[p].shape[1])))
+            strides.append(int(np.sqrt(img_h * img_w / preds[p].shape[1])))
 
     for i, s in enumerate(strides):
         print("s: " + str(strides[i]))